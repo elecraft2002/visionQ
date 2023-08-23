bf8a9b49ec4b233ddad10d2eb3e5d824f00f3ca2
Vojtech Suchanek <suchavo3@cvut.cz>
2023-08-23 11:22:10 +0200

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "localhost:3000",
+  siteUrl: process.env.SITE_URL || "https://www.visionq.cz",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
